5dfe6c0e7e5e3ae48450f49285d65348dbc3a00c
Frank Calise <fcalise@gmail.com>
2022-09-29 15:52:35 -0400

    feat: initial commit! 🔥

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,87 @@
+import { observer } from "mobx-react-lite"
+import React, {
+} from "react"
+import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import {
+  Text,
+} from "../components"
+import { isRTL } from "../i18n"
+import { colors, spacing } from "../theme"
+
+const welcomeLogo = require("../../assets/images/logo.png")
+const welcomeFace = require("../../assets/images/welcome-face.png")
+
+
+export const WelcomeScreen = observer(function WelcomeScreen(
+) {
+
+  return (
+    <View style={$container}>
+      <View style={$topContainer}>
+        <Image style={$welcomeLogo} source={welcomeLogo} resizeMode="contain" />
+        <Text
+          testID="welcome-heading"
+          style={$welcomeHeading}
+          tx="welcomeScreen.readyForLaunch"
+          preset="heading"
+        />
+        <Text tx="welcomeScreen.exciting" preset="subheading" />
+        <Image style={$welcomeFace} source={welcomeFace} resizeMode="contain" />
+      </View>
+
+      <SafeAreaView style={$bottomContainer} edges={["bottom"]}>
+        <View style={$bottomContentContainer}>
+          <Text tx="welcomeScreen.postscript" size="md" />
+        </View>
+      </SafeAreaView>
+    </View>
+  )
+})
+
+const $container: ViewStyle = {
+  flex: 1,
+  backgroundColor: colors.background,
+}
+
+const $topContainer: ViewStyle = {
+  flexShrink: 1,
+  flexGrow: 1,
+  flexBasis: "57%",
+  justifyContent: "center",
+  paddingHorizontal: spacing.large,
+}
+
+const $bottomContainer: ViewStyle = {
+  flexShrink: 1,
+  flexGrow: 0,
+  flexBasis: "43%",
+  backgroundColor: colors.palette.neutral100,
+  borderTopLeftRadius: 16,
+  borderTopRightRadius: 16,
+}
+
+const $bottomContentContainer: ViewStyle = {
+  flex: 1,
+  paddingHorizontal: spacing.large,
+  justifyContent: "space-around",
+}
+
+const $welcomeLogo: ImageStyle = {
+  height: 88,
+  width: "100%",
+  marginBottom: spacing.huge,
+}
+
+const $welcomeFace: ImageStyle = {
+  height: 169,
+  width: 269,
+  position: "absolute",
+  bottom: -47,
+  right: -80,
+  transform: [{ scaleX: isRTL ? -1 : 1 }],
+}
+
+const $welcomeHeading: TextStyle = {
+  marginBottom: spacing.medium,
+}
