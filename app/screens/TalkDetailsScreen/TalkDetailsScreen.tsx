62af1571dac950232fe82b4d2dd3a4e409ec7fc5
Frank Calise <fcalise@gmail.com>
2022-11-29 18:21:50 -0500

    feat(talk-details): WIP animated header

---
+++ app/screens/TalkDetailsScreen/TalkDetailsScreen.tsx
@@ -0,0 +1,146 @@
+import React, { FC } from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle, View, TextStyle, ImageStyle, Image, StyleSheet } from "react-native"
+import { StackScreenProps } from "@react-navigation/stack"
+import { AppStackParamList } from "../../navigators"
+import { Screen, Text, Tag, IconButton, MIN_HEADER_HEIGHT } from "../../components"
+import { colors, spacing } from "../../theme"
+import { useAppNavigation, useHeader } from "../../hooks"
+import { openLinkInBrowser } from "../../utils/openLinkInBrowser"
+import { TalkDetailsHeader } from "./TalksDetailsHeader"
+import Animated, { useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated"
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context"
+
+export const TalkDetailsScreen: FC<StackScreenProps<AppStackParamList, "TalkDetails">> = observer(
+  function TalkDetailsScreen() {
+    const navigation = useAppNavigation()
+    // useHeader(
+    //   {
+    //     title: "Talk Title",
+    //     leftIcon: "back",
+    //     onLeftPress: navigation.goBack,
+    //   },
+    //   [navigation],
+    // )
+    const scrollY = useSharedValue(0)
+    const onPress = () => openLinkInBrowser("https://infinite.red")
+
+    const scrollHandler = useAnimatedScrollHandler({
+      onScroll: (event) => {
+        scrollY.value = event.contentOffset.y
+      },
+    })
+
+    const { bottom: paddingBottom } = useSafeAreaInsets()
+
+    return (
+      <View style={{ flex: 1 }}>
+        <TalkDetailsHeader
+          title="Talk Title"
+          subtitle="Subtitle"
+          onPress={navigation.goBack}
+          scrollY={scrollY}
+        />
+        <Animated.ScrollView
+          style={[$scrollView, { paddingBottom }]}
+          // preset="scroll"
+          scrollEventThrottle={16}
+          // safeAreaEdges={["bottom"]}
+          onScroll={scrollHandler}
+        >
+          <View style={$container}>
+            <View style={$containerSpacing}>
+              <Image source={{ uri: "https://picsum.photos/315" }} style={$speakerImage} />
+              <Text preset="bold" style={$nameText} text="First Last" />
+              <Text style={$companyNameText} text="Company, Inc" />
+            </View>
+
+            <View style={$detailsContainer}>
+              <Text preset="bold" style={$detailsText} text="Workshop details" />
+              <Tag text="Beginner Workshop" style={$containerSpacing} />
+              <Text
+                style={$bodyText}
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              />
+            </View>
+
+            <View style={$containerSpacing}>
+              <Text preset="eventTitle" style={$aboutHeading} text="About First" />
+              <Text
+                style={$bodyText}
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              />
+            </View>
+
+            <View style={$linksContainer}>
+              <IconButton icon="twitter" onPress={onPress} />
+              <IconButton icon="github" onPress={onPress} />
+              <IconButton icon="link" onPress={onPress} />
+            </View>
+          </View>
+        </Animated.ScrollView>
+      </View>
+    )
+  },
+)
+
+const $scrollView: ViewStyle = {
+  paddingTop: MIN_HEADER_HEIGHT + spacing.large + 50,
+  backgroundColor: colors.background,
+}
+
+const $container = {
+  padding: spacing.large,
+}
+
+const $containerSpacing: ViewStyle = {
+  marginBottom: spacing.large,
+}
+
+const $linksContainer: ViewStyle = {
+  flexDirection: "row",
+  justifyContent: "space-between",
+  width: "50%",
+}
+
+const $nameText: TextStyle = {
+  fontSize: 22,
+  lineHeight: 24.2,
+  marginBottom: spacing.tiny,
+}
+
+const $subheadingColor: TextStyle = {
+  color: colors.palette.primary500,
+}
+
+const $detailsContainer: ViewStyle = {
+  marginVertical: spacing.large,
+}
+
+const $speakerImage: ImageStyle = {
+  height: 315,
+  marginBottom: spacing.medium,
+}
+
+const $companyNameText: TextStyle = {
+  ...$subheadingColor,
+  fontSize: 16,
+  lineHeight: 22.4,
+}
+
+const $detailsText: TextStyle = {
+  fontSize: 26,
+  lineHeight: 28.6,
+  ...$containerSpacing,
+}
+
+const $aboutHeading: TextStyle = {
+  ...$containerSpacing,
+  color: colors.palette.primary500,
+  textTransform: "uppercase",
+}
+
+const $bodyText: TextStyle = {
+  fontSize: 16,
+  lineHeight: 22.4,
+}
---
+++ app/screens/TalkDetailsScreen/TalksDetailsHeader.tsx
@@ -0,0 +1,133 @@
+import React from "react"
+import {
+  Dimensions,
+  StyleSheet,
+  TextStyle,
+  TouchableOpacityProps,
+  View,
+  ViewStyle,
+} from "react-native"
+import Animated, {
+  Extrapolate,
+  interpolate,
+  SharedValue,
+  useAnimatedStyle,
+  useSharedValue,
+  useValue,
+} from "react-native-reanimated"
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context"
+import { HeaderAction, MIN_HEADER_HEIGHT, Text } from "../../components"
+import { colors, spacing, typography } from "../../theme"
+
+interface TalkDetailsHeaderProps {
+  onPress?: TouchableOpacityProps["onPress"]
+  title: string
+  subtitle: string
+  scrollY: SharedValue<number>
+}
+
+const { width } = Dimensions.get("screen")
+console.log(width, width / 2)
+
+export const TalkDetailsHeader: React.FunctionComponent<TalkDetailsHeaderProps> =
+  function TalkDetailsHeader({ onPress, title, subtitle, scrollY }) {
+    const { top } = useSafeAreaInsets()
+    const titleHeight = useSharedValue(0)
+    const titleWidth = useSharedValue(0)
+    // const toggle = useValue<0 | 1>(0);
+    // const transition = withTimingTransition (toggle, { duration: 100 })
+    const $animatedTitle = useAnimatedStyle(() => {
+      const translateY = interpolate(
+        scrollY.value,
+        [0, MIN_HEADER_HEIGHT],
+        [MIN_HEADER_HEIGHT, 0],
+        Extrapolate.CLAMP,
+      )
+
+      // output range, second param
+      // -spacing.large due to wrapper view (can adjust header action button with override to get rid of this)
+      // -12 for half of back arrow
+      // half screen width
+      // half title label width
+      const translateX = interpolate(
+        scrollY.value,
+        [0, MIN_HEADER_HEIGHT],
+        [0, -spacing.large - 12 + width / 2 - titleWidth.value / 2],
+        Extrapolate.CLAMP,
+      )
+
+      const fontSize = interpolate(
+        scrollY.value,
+        [0, MIN_HEADER_HEIGHT],
+        [32, 16],
+        Extrapolate.CLAMP,
+      )
+
+      // const marginLeft = interpolate(
+      //   scrollY.value,
+      //   [0, MIN_HEADER_HEIGHT],
+      //   [spacing.large, -spacing.large],
+      //   Extrapolate.CLAMP,
+      // )
+
+      return { transform: [{ translateX }, { translateY }], fontSize }
+    })
+
+    return (
+      <Animated.View style={[$safeArea, { paddingTop: top }]}>
+        <View style={$rowContainer}>
+          <View style={{ marginHorizontal: -spacing.large }}>
+            <HeaderAction icon="back" {...{ onPress }} />
+          </View>
+
+          <Animated.Text
+            style={[$title, $animatedTitle]}
+            onLayout={({
+              nativeEvent: {
+                layout: { width, height },
+              },
+            }) => {
+              titleHeight.value = height
+              titleWidth.value = width
+            }}
+          >
+            {title}
+          </Animated.Text>
+        </View>
+        {/* <Text preset="companionHeading" style={$subtitle} text={subtitle} /> */}
+      </Animated.View>
+    )
+  }
+
+const $safeArea: ViewStyle = {
+  backgroundColor: colors.background,
+  position: "absolute",
+  zIndex: 2,
+  top: 0,
+  right: 0,
+  left: spacing.large,
+  paddingBottom: spacing.extraLarge,
+}
+
+const $rowContainer: ViewStyle = {
+  flexDirection: "row",
+  alignItems: "center",
+  // paddingHorizontal: spacing.extraSmall,
+}
+
+const $title: TextStyle = {
+  fontSize: 32,
+  lineHeight: 35.2,
+  // flex: 1,
+  // justifyContent: "center",
+  fontFamily: typography.primary.bold,
+  color: colors.text,
+  // textAlign: "center",
+  // width: "100%",
+  // marginBottom: spacing.extraSmall,
+}
+
+const $subtitle: TextStyle = {
+  color: colors.palette.primary500,
+  // marginBottom: spacing.extraLarge,
+}
