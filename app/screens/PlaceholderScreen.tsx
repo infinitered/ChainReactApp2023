5c6e582d25261e4ab7ca7dd9bbe628fbd570ffa9
Mazen Chami <mazenchami@gmail.com>
2022-12-06 11:37:53 -0500

    (feat) removing MST & Apisauce (#23 by @mazenchami)

--- app/screens/PlaceholderScreen.tsx
+++ app/screens/PlaceholderScreen.tsx
@@ -1,24 +1,21 @@
 import React, { FC } from "react"
-import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { Screen, Text } from "../components"
 import { TabScreenProps } from "../navigators/TabNavigator"
 import { spacing } from "../theme"
 
-export const PlaceholderScreen: FC<TabScreenProps<"Schedule">> = observer(
-  function PlaceholderScreen() {
-    return (
-      <Screen
-        style={$root}
-        preset="scroll"
-        safeAreaEdges={["top", "bottom"]}
-        contentContainerStyle={$container}
-      >
-        <Text>Placeholder!</Text>
-      </Screen>
-    )
-  },
-)
+export const PlaceholderScreen: FC<TabScreenProps<"Schedule">> = () => {
+  return (
+    <Screen
+      style={$root}
+      preset="scroll"
+      safeAreaEdges={["top", "bottom"]}
+      contentContainerStyle={$container}
+    >
+      <Text>Placeholder!</Text>
+    </Screen>
+  )
+}
 
 const $root: ViewStyle = {
   flex: 1,
--- app/screens/TalkDetailsScreen.tsx
+++ app/screens/TalkDetailsScreen.tsx
@@ -1,26 +1,23 @@
 import React, { FC } from "react"
-import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { AppStackParamList } from "../navigators"
 import { Screen, Text, Tag, IconButton } from "../components"
 
-export const TalkDetailsScreen: FC<StackScreenProps<AppStackParamList, "TalkDetails">> = observer(
-  function TalkDetailsScreen() {
-    const onPress = () => console.log("pressed")
-    return (
-      <Screen style={$root} preset="scroll" safeAreaEdges={["top", "bottom"]}>
-        <Text text="talkDetails" />
+export const TalkDetailsScreen: FC<StackScreenProps<AppStackParamList, "TalkDetails">> = () => {
+  const onPress = () => console.log("pressed")
+  return (
+    <Screen style={$root} preset="scroll" safeAreaEdges={["top", "bottom"]}>
+      <Text text="talkDetails" />
 
-        <IconButton icon="twitter" onPress={onPress} />
-        <IconButton icon="github" onPress={onPress} />
-        <IconButton icon="link" onPress={onPress} />
+      <IconButton icon="twitter" onPress={onPress} />
+      <IconButton icon="github" onPress={onPress} />
+      <IconButton icon="link" onPress={onPress} />
 
-        <Tag text="Beginner Workshop" />
-      </Screen>
-    )
-  },
-)
+      <Tag text="Beginner Workshop" />
+    </Screen>
+  )
+}
 
 const $root: ViewStyle = {
   flex: 1,
