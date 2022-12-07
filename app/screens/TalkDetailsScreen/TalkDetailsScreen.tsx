1f2ddd00c363d44fc99b4d9752fa3e1de38ce896
Frank Calise <fcalise@gmail.com>
2022-12-07 10:45:14 -0500

    fix(talk-details-screen): remove observer

--- app/screens/TalkDetailsScreen/TalkDetailsScreen.tsx
+++ app/screens/TalkDetailsScreen/TalkDetailsScreen.tsx
@@ -17,98 +17,96 @@ const talkCurve = require("../../../assets/images/talk-curve.png")
 const title = "React Native essentials"
 const subtitle = "Hotel, Conference Room 1"
 
-export const TalkDetailsScreen: FC<StackScreenProps<AppStackParamList, "TalkDetails">> = observer(
-  function TalkDetailsScreen() {
-    const scrollY = useSharedValue(0)
-    const onPress = (url) => openLinkInBrowser(url)
-    const [headingHeight, setHeadingHeight] = React.useState(0)
+export const TalkDetailsScreen: FC<StackScreenProps<AppStackParamList, "TalkDetails">> = () => {
+  const scrollY = useSharedValue(0)
+  const onPress = (url) => openLinkInBrowser(url)
+  const [headingHeight, setHeadingHeight] = React.useState(0)
 
-    const scrollHandler = useAnimatedScrollHandler({
-      onScroll: (event) => {
-        scrollY.value = event.contentOffset.y
-      },
-    })
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollY.value = event.contentOffset.y
+    },
+  })
 
-    const { bottom: paddingBottom } = useSafeAreaInsets()
+  const { bottom: paddingBottom } = useSafeAreaInsets()
 
-    // TODO: wire this up to the event type
-    const isWorkshop = true
+  // TODO: wire this up to the event type
+  const isWorkshop = true
 
-    return (
-      <Screen safeAreaEdges={["top", "bottom"]} style={$root}>
-        <TalkDetailsHeader {...{ title, subtitle, scrollY, headingHeight }} />
+  return (
+    <Screen safeAreaEdges={["top", "bottom"]} style={$root}>
+      <TalkDetailsHeader {...{ title, subtitle, scrollY, headingHeight }} />
 
-        <Animated.ScrollView
-          style={[$scrollView, { paddingBottom }]}
-          scrollEventThrottle={16}
-          onScroll={scrollHandler}
-          showsVerticalScrollIndicator={false}
-        >
-          <View style={$container}>
-            <View style={$headingContainer}>
-              <Text
-                preset="heading"
-                style={$title}
-                text={title}
-                onLayout={({
-                  nativeEvent: {
-                    layout: { height },
-                  },
-                }) => {
-                  setHeadingHeight(height)
-                }}
-              />
-              <Text preset="companionHeading" style={$subtitle} text={subtitle} />
-            </View>
-            <View style={$containerSpacing}>
-              <Image
-                source={isWorkshop ? workshopCurve : talkCurve}
-                style={isWorkshop ? $workshopCurve : $talkCurve}
-              />
-              <BoxShadow
-                preset={isWorkshop ? "bold" : "primary"}
-                style={$containerSpacing}
-                offset={6}
-              >
-                <Image source={{ uri: "https://picsum.photos/315" }} style={$speakerImage} />
-              </BoxShadow>
-              <Image
-                source={isWorkshop ? workshopBlob : talkBlob}
-                style={isWorkshop ? $workshopBlob : $talkBlob}
-              />
-
-              <Text preset="bold" style={$nameText} text="First Last" />
-              <Text style={$companyNameText} text="Company, Inc" />
-            </View>
-
-            <View style={$detailsContainer}>
-              <Text preset="bold" style={$detailsText} text="Workshop details" />
-              <Tag text="Beginner Workshop" style={$containerSpacing} />
-              <Text
-                style={$bodyText}
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-              />
-            </View>
-
-            <View style={$containerSpacing}>
-              <Text preset="eventTitle" style={$aboutHeading} text="About First" />
-              <Text
-                style={$bodyText}
-                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-              />
-            </View>
-
-            <View style={$linksContainer}>
-              <IconButton icon="twitter" onPress={() => onPress("https://cr.infinite.red")} />
-              <IconButton icon="github" onPress={() => onPress("https://cr.infinite.red")} />
-              <IconButton icon="link" onPress={() => onPress("https://cr.infinite.red")} />
-            </View>
+      <Animated.ScrollView
+        style={[$scrollView, { paddingBottom }]}
+        scrollEventThrottle={16}
+        onScroll={scrollHandler}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={$container}>
+          <View style={$headingContainer}>
+            <Text
+              preset="heading"
+              style={$title}
+              text={title}
+              onLayout={({
+                nativeEvent: {
+                  layout: { height },
+                },
+              }) => {
+                setHeadingHeight(height)
+              }}
+            />
+            <Text preset="companionHeading" style={$subtitle} text={subtitle} />
           </View>
-        </Animated.ScrollView>
-      </Screen>
-    )
-  },
-)
+          <View style={$containerSpacing}>
+            <Image
+              source={isWorkshop ? workshopCurve : talkCurve}
+              style={isWorkshop ? $workshopCurve : $talkCurve}
+            />
+            <BoxShadow
+              preset={isWorkshop ? "bold" : "primary"}
+              style={$containerSpacing}
+              offset={6}
+            >
+              <Image source={{ uri: "https://picsum.photos/315" }} style={$speakerImage} />
+            </BoxShadow>
+            <Image
+              source={isWorkshop ? workshopBlob : talkBlob}
+              style={isWorkshop ? $workshopBlob : $talkBlob}
+            />
+
+            <Text preset="bold" style={$nameText} text="First Last" />
+            <Text style={$companyNameText} text="Company, Inc" />
+          </View>
+
+          <View style={$detailsContainer}>
+            <Text preset="bold" style={$detailsText} text="Workshop details" />
+            <Tag text="Beginner Workshop" style={$containerSpacing} />
+            <Text
+              style={$bodyText}
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            />
+          </View>
+
+          <View style={$containerSpacing}>
+            <Text preset="eventTitle" style={$aboutHeading} text="About First" />
+            <Text
+              style={$bodyText}
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            />
+          </View>
+
+          <View style={$linksContainer}>
+            <IconButton icon="twitter" onPress={() => onPress("https://cr.infinite.red")} />
+            <IconButton icon="github" onPress={() => onPress("https://cr.infinite.red")} />
+            <IconButton icon="link" onPress={() => onPress("https://cr.infinite.red")} />
+          </View>
+        </View>
+      </Animated.ScrollView>
+    </Screen>
+  )
+}
 
 const $root: ViewStyle = {
   backgroundColor: colors.background,
