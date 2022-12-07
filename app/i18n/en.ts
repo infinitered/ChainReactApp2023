cfcd65012705a1ffe2b6661038195cd6bbabf412
Frank Calise <fcalise@gmail.com>
2022-12-07 08:57:40 -0500

    feat(info-screen): implemented screen design and structure

--- app/i18n/en.ts
+++ app/i18n/en.ts
@@ -39,6 +39,27 @@ const en = {
     silverSponsor: "Silver Sponsors",
     bronzeSponsor: "Bronze Sponsors",
   },
+  infoScreen: {
+    title: "Info",
+    screenHeading: "Hosted by the Infinite Red team",
+    aboutTitle: "About Chain React",
+    about:
+      "Sometimes called the “Fifth Quadrant,” North Portland encompasses a range of neighborhoods and commercial districts. Some places to visit or things to see include: St John’s Bridge, Cathedral Park, or shopping and dining on Mississippi",
+    emailLink: "Email our team",
+    conductWarning:
+      "All attendees, speakers, sponsors and volunteers at our conference are required to agree with the following code of conduct. Organizers will enforce this code throughout the event. We expect cooperation from all participants to help ensure a safe environment for everybody.",
+    codeOfConductTitle: "Our Code of Conduct Policy",
+    codeOfConduct:
+      "Our conference is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks, workshops, parties, Twitter and other online media. Conference participants violating these rules may be sanctioned or expelled from the conference without a refund at the discretion of the conference organizers.",
+    extraDetailsTitle: "Extra Details",
+    extraDetails:
+      "Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity religion, technology choices, sexual images in public spaces, deliberate intimidation, stalking following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention. Participants asked to stop any harassing behavior are expected to comply immediately. Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not use sexualised images, activities, or other material. Booth staff (including volunteers) should not use sexualised clothing/uniforms/costumes, or otherwise create a sexualised environment. If a participant engages in harassing behavior, the conference organisers may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund. If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of conference staff immediately. Conference staff can be identified as they'll be wearing branded clothing and/or badges. Conference staff will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the conference. We value your attendance. We expect participants to follow these rules at conference and workshop venues and conference-related social events.",
+    reportingIncidentTitle: "Reporting an Incident",
+    reportingIncidentPart1:
+      "If you have a question, or experience a situation that makes you feel unsafe, please don't hesitate to reach out to us at ",
+    reportingIncidentPart2:
+      ".\n\nWhen submitting a report, please include as much information as you feel comfortable sharing. We take all reports seriously.",
+  },
 }
 
 export default en
