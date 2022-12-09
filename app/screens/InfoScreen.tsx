d013d24cb0aa27306f5203851f34c3084e58d01d
Frank Calise <fcalise@gmail.com>
2022-12-09 09:56:15 -0500

    fix(info-screen): design updates and animations (#26 by @frankcalise)
    * vendor: removed expo-modules-core
    
    * fix(info-screen): moved email link as per designs
    
    * feat(info-screen): parallax carousel animation
    
    * chore: lint fix
    
    * fix(info-screen): switched to reanimated for parallax
    
    * chore: notes about the animated flatlist hiccup

--- app/screens/InfoScreen.tsx
+++ app/screens/InfoScreen.tsx
@@ -4,16 +4,22 @@ import {
   TextStyle,
   View,
   ViewStyle,
-  FlatList,
   ImageSourcePropType,
   Image,
+  FlatList,
 } from "react-native"
-import { CarouselCard, Screen, Text } from "../components"
+import { Button, Screen, Text } from "../components"
 import { useHeader } from "../hooks"
 import { TabScreenProps } from "../navigators/TabNavigator"
 import { colors, spacing } from "../theme"
 import { translate } from "../i18n"
 import { openLinkInBrowser } from "../utils/openLinkInBrowser"
+import Animated, {
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated"
 
 const IMAGE_WIDTH = 358
 
@@ -26,11 +32,39 @@ const irImage3 = require("../../assets/images/info-ir3.png")
 
 const carouselImages: ImageSourcePropType[] = [irImage1, irImage2, irImage3]
 
-export const InfoScreen: React.FunctionComponent<TabScreenProps<"Info">> = () => {
-  useHeader({ title: translate("infoScreen.title") })
+// ! https://github.com/software-mansion/react-native-reanimated/issues/457
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
 
+const CarouselItem = ({ item, index, scrollX }) => {
+  const $animatedImage = useAnimatedStyle(() => {
+    const inputRange = [(index - 1) * IMAGE_WIDTH, index * IMAGE_WIDTH, (index + 1) * IMAGE_WIDTH]
+
+    const scale = interpolate(scrollX.value, inputRange, [1, 1.1, 1])
+
+    return { transform: [{ scale }] }
+  })
+
+  return (
+    <View style={$cardWrapper}>
+      <Animated.Image source={item} style={$animatedImage} />
+    </View>
+  )
+}
+
+export const InfoScreen: React.FunctionComponent<TabScreenProps<"Info">> = () => {
   // NOTE: this only works on a device, warning in sim
   const callPhoneNumber = () => openLinkInBrowser(`tel:${phoneNumber}`)
+  const contactByEmail = () => openLinkInBrowser("mailto:conf@infinite.red")
+
+  useHeader({
+    title: translate("infoScreen.title"),
+    RightActionComponent: (
+      <Button preset="link" text="Contact" onPress={contactByEmail} style={$emailButton} />
+    ),
+  })
+
+  const scrollX = useSharedValue(0)
+  const scrollHandler = useAnimatedScrollHandler((event) => (scrollX.value = event.contentOffset.x))
 
   return (
     <Screen style={$root} preset="scroll" ScrollViewProps={{ showsVerticalScrollIndicator: false }}>
@@ -38,34 +72,27 @@ export const InfoScreen: React.FunctionComponent<TabScreenProps<"Info">> = () =>
         <Text preset="screenHeading" tx="infoScreen.screenHeading" />
       </View>
 
-      <FlatList
+      <AnimatedFlatList
         data={carouselImages}
+        onScroll={scrollHandler}
         keyExtractor={(_, index) => `info-image-${index}`}
         bounces={false}
         showsHorizontalScrollIndicator={false}
         horizontal
         pagingEnabled
         decelerationRate="fast"
+        scrollEventThrottle={16}
         snapToInterval={IMAGE_WIDTH}
         style={$carousel}
         contentContainerStyle={$carouselContent}
-        renderItem={({ item }) => {
-          return (
-            <View style={$cardWrapper}>
-              <Image source={item} />
-            </View>
-          )
-        }}
+        renderItem={({ item, index }) => <CarouselItem {...{ item, index, scrollX }} />}
       />
 
       <View style={$content}>
         <Text preset="subheading" tx="infoScreen.aboutTitle" style={$heading} />
         <Text preset="infoText" tx="infoScreen.about" />
 
-        <View style={$ctaContainer}>
-          <CarouselCard.Link text="Email our team" openLink={() => console.log("email team")} />
-        </View>
-
+        <Text preset="screenHeading" text="Code of conduct" style={$codeOfConductHeading} />
         <Text preset="infoText" tx="infoScreen.conductWarning" />
       </View>
 
@@ -101,18 +128,13 @@ const $root: ViewStyle = {
 }
 
 const $cardWrapper: ViewStyle = {
+  overflow: "hidden",
+  borderRadius: 4,
   marginEnd: spacing.extraSmall,
-  width: IMAGE_WIDTH,
 }
 
 const $heading: TextStyle = {
   marginBottom: spacing.extraSmall,
-}
-
-const $ctaContainer: ViewStyle = {
-  flexDirection: "row",
-  marginTop: spacing.extraSmall,
-  marginBottom: spacing.large,
 }
 
 const $carousel: ViewStyle = {
@@ -133,7 +155,6 @@ const $imageContainer: ViewStyle = {
 }
 
 const $infoImage: ImageStyle = {
-  borderRadius: 4,
   marginHorizontal: spacing.extraSmall,
 }
 
@@ -144,4 +165,13 @@ const $mb: ViewStyle = {
 const $phoneNumber: TextStyle = {
   textDecorationLine: "underline",
   textDecorationColor: colors.text,
+}
+
+const $codeOfConductHeading: TextStyle = {
+  marginTop: spacing.extraLarge * 2,
+  marginBottom: spacing.extraSmall,
+}
+
+const $emailButton: ViewStyle = {
+  marginEnd: spacing.medium,
 }
