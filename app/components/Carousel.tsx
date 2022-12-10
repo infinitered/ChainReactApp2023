fcca24d3f1613e5b8861a6f365f813e38e4a9171
Frank Calise <fcalise@gmail.com>
2022-12-09 23:51:21 -0500

    feat(components): added new Carousel component

---
+++ app/components/Carousel.tsx
@@ -0,0 +1,152 @@
+import React from "react"
+import { View, FlatList, Dimensions, ViewStyle, ImageSourcePropType, TextStyle } from "react-native"
+import { Text } from "../components"
+import { spacing } from "../theme"
+import Animated, {
+  interpolate,
+  SharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated"
+
+interface StaticCarouselItem {
+  subtitle?: string
+  meta?: string
+  body: string
+}
+
+interface BaseCarouselProps {
+  title: string
+}
+
+interface StaticCarouselProps extends BaseCarouselProps, StaticCarouselItem {
+  preset: "static"
+  data: ImageSourcePropType[]
+}
+
+interface DynamicCarouselItem extends StaticCarouselItem {
+  image: ImageSourcePropType
+}
+
+interface DynamicCarouselProps extends BaseCarouselProps {
+  preset: "dynamic"
+  data: DynamicCarouselItem[]
+}
+
+type CarouselProps = StaticCarouselProps | DynamicCarouselProps
+
+interface CarouselItemProps {
+  item: ImageSourcePropType | DynamicCarouselItem
+  index: number
+  scrollX: SharedValue<number>
+}
+
+const IMAGE_WIDTH = Dimensions.get("screen").width - spacing.medium
+const AnimatedText = Animated.createAnimatedComponent(Text)
+
+function CarouselItem(props: CarouselItemProps) {
+  const { item, index, scrollX } = props
+  const { subtitle, meta, body, image } = item as DynamicCarouselItem
+  const source = subtitle ? image : item
+
+  const inputRange = [(index - 1) * IMAGE_WIDTH, index * IMAGE_WIDTH, (index + 1) * IMAGE_WIDTH]
+
+  const $animatedImage = useAnimatedStyle(() => {
+    const scale = interpolate(scrollX.value, inputRange, [1, 1.1, 1])
+    return { transform: [{ scale }] }
+  })
+
+  const $imageStyle = { width: IMAGE_WIDTH - spacing.medium }
+
+  const $animatedText = useAnimatedStyle(() => {
+    const translateX = interpolate(scrollX.value, inputRange, [IMAGE_WIDTH, -0, -IMAGE_WIDTH])
+    return { transform: [{ translateX }] }
+  })
+
+  return (
+    <View>
+      <View style={$cardWrapper}>
+        <Animated.Image source={source} style={[$imageStyle, $animatedImage]} />
+      </View>
+      {!!image && (
+        <View style={[{ width: IMAGE_WIDTH - spacing.medium }, $mt]}>
+          {!!meta && (
+            <AnimatedText preset="primaryLabel" text={meta} style={[$mb, $animatedText]} />
+          )}
+          <AnimatedText preset="subheading" text={subtitle} style={[$mb, $animatedText]} />
+          <AnimatedText text={body} style={$animatedText} />
+        </View>
+      )}
+    </View>
+  )
+}
+
+// ! https://github.com/software-mansion/react-native-reanimated/issues/457
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
+
+export function Carousel(props: CarouselProps) {
+  const { title, data } = props
+  const { subtitle, meta, body } = props as StaticCarouselProps
+
+  const scrollX = useSharedValue(0)
+  const onScroll = useAnimatedScrollHandler((event) => (scrollX.value = event.contentOffset.x))
+
+  return (
+    <>
+      <View style={$content}>
+        <Text preset="screenHeading" text={title} />
+      </View>
+
+      <AnimatedFlatList
+        {...{ data, onScroll }}
+        horizontal
+        pagingEnabled
+        keyExtractor={(_, index) => `info-image-${index}`}
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        decelerationRate="fast"
+        scrollEventThrottle={16}
+        snapToInterval={IMAGE_WIDTH - spacing.medium}
+        style={$carousel}
+        contentContainerStyle={$carouselContent}
+        renderItem={({ item, index }) => <CarouselItem {...{ item, index, scrollX }} />}
+      />
+
+      {!subtitle && (
+        <View style={$content}>
+          {!meta && <Text preset="primaryLabel" text={meta} style={$mb} />}
+          <Text preset="subheading" text={subtitle} style={$mb} />
+          <Text text={body} />
+        </View>
+      )}
+    </>
+  )
+}
+
+const $cardWrapper: ViewStyle = {
+  overflow: "hidden",
+  borderRadius: 4,
+  marginEnd: spacing.extraSmall,
+}
+
+const $carousel: ViewStyle = {
+  paddingStart: spacing.extraSmall,
+  marginVertical: spacing.medium,
+}
+
+const $carouselContent: ViewStyle = {
+  paddingEnd: spacing.extraSmall,
+}
+
+const $content: ViewStyle = {
+  paddingHorizontal: spacing.large,
+}
+
+const $mb: TextStyle = {
+  marginBottom: spacing.extraSmall,
+}
+
+const $mt: ViewStyle = {
+  marginTop: spacing.extraSmall,
+}
