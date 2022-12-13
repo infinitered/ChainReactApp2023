73c65d4d6217db54c7c4c11408f5f9e152d76b80
Frank Calise <fcalise@gmail.com>
2022-12-13 12:59:55 -0500

    feat(venue-screen): utilize shared Carousel

--- app/screens/VenueScreen/VenueScreen.tsx
+++ app/screens/VenueScreen/VenueScreen.tsx
@@ -1,26 +1,45 @@
 import React, { FC } from "react"
-import { SectionList, TextStyle, View, ViewStyle } from "react-native"
-import { CarouselCard, Screen, Text } from "../../components"
+import { FlatList, TextStyle, View, ViewStyle } from "react-native"
+import { Carousel, DynamicCarouselItem, Screen, Text } from "../../components"
 import { TabScreenProps } from "../../navigators/TabNavigator"
 import { colors, spacing } from "../../theme"
 import { useHeader } from "../../hooks/useHeader"
 import { translate } from "../../i18n"
-import { openMap } from "../../utils/openMap"
 import { SponsorCard } from "./SponsorCard"
 
+const irImage1 = require("../../../assets/images/info-ir1.png")
+const irImage2 = require("../../../assets/images/info-ir2.png")
+
+const WORKSHOP_DATA: DynamicCarouselItem[] = [
+  {
+    image: irImage1,
+    subtitle: "The Armory",
+    meta: "Conference • May 18-19",
+    body: "11134 Washington St #302\nPortland, OR 97006",
+    leftButton: {
+      text: "Open in maps",
+      link: "11134 Washington St #302, Portland, OR 97006",
+    },
+  },
+  {
+    image: irImage2,
+    subtitle: "Workshops • May 17",
+    meta: "Marriott Portland City Center",
+    body: "550 SW Oak Street\nPortland, OR 97204",
+    leftButton: {
+      text: "Open in maps",
+      link: "550 SW Oak Street\nPortland, OR 97204",
+    },
+  },
+]
+
 const Workshops = () => {
-  const openLink = async () => {
-    await openMap("11134 Washington St #302", "97006", "Portland")
-  }
   return (
     <View style={$carousel}>
-      {/* test carousel card */}
-      <CarouselCard
-        coverImage={{ uri: "https://placekitten.com/g/358/274" }}
-        meta="Conference • May 18-19"
-        title="The Armory"
-        description="11134 Washington St #302, Portland, OR 97006"
-        leftButton={<CarouselCard.Link text="Open in maps" openLink={openLink} />}
+      <Carousel
+        preset="dynamic"
+        data={WORKSHOP_DATA}
+        title={translate("venueScreen.conferenceAndWorkshopVenues")}
       />
     </View>
   )
@@ -30,67 +49,56 @@ const SponsorSection = ({ item }) => {
   return <SponsorCard {...item} />
 }
 
-const SECTIONS = [
+const SPONSORS_DATA = [
   {
-    title: translate("venueScreen.conferenceAndWorkshopVenues"),
-    renderItem: Workshops,
-    data: [{}],
+    sponsor: "Sponsor 1",
+    tier: "platinum",
+    bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
+    openURL: "https://placekitten.com",
+    sponsorImage: {
+      uri: "https://placekitten.com/g/280/60",
+    },
   },
   {
-    title: translate("venueScreen.thanksToThisYearsSponsors"),
-    renderItem: SponsorSection,
-    data: [
-      {
-        sponsor: "Sponsor 1",
-        tier: "platinum",
-        bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
-        openURL: "https://placekitten.com",
-        sponsorImage: {
-          uri: "https://placekitten.com/g/280/60",
-        },
-      },
-      {
-        sponsor: "Sponsor 2",
-        tier: "gold",
-        bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
-        openURL: "https://placekitten.com",
-        sponsorImage: {
-          uri: "https://placekitten.com/g/280/60",
-        },
-      },
-      {
-        sponsor: "Sponsor 3",
-        tier: "gold",
-        bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
-        openURL: "https://placekitten.com",
-        sponsorImage: {
-          uri: "https://placekitten.com/g/280/60",
-        },
-      },
-      {
-        tier: "silver",
-        sponsorImages: [
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-        ],
-      },
-      {
-        tier: "bronze",
-        sponsorImages: [
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-        ],
-      },
+    sponsor: "Sponsor 2",
+    tier: "gold",
+    bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
+    openURL: "https://placekitten.com",
+    sponsorImage: {
+      uri: "https://placekitten.com/g/280/60",
+    },
+  },
+  {
+    sponsor: "Sponsor 3",
+    tier: "gold",
+    bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
+    openURL: "https://placekitten.com",
+    sponsorImage: {
+      uri: "https://placekitten.com/g/280/60",
+    },
+  },
+  {
+    tier: "silver",
+    sponsorImages: [
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+    ],
+  },
+  {
+    tier: "bronze",
+    sponsorImages: [
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
     ],
   },
 ]
@@ -99,14 +107,21 @@ export const VenueScreen: FC<TabScreenProps<"Venue">> = () => {
   useHeader({ title: translate("venueScreen.title") })
 
   return (
-    <Screen style={$root} preset="fixed" contentContainerStyle={$container}>
-      <SectionList
-        stickySectionHeadersEnabled
-        sections={SECTIONS}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text preset="screenHeading" style={$heading}>
-            {title}
-          </Text>
+    <Screen style={$root} preset="fixed">
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <Workshops />
+            <Text preset="screenHeading" style={$heading}>
+              {translate("venueScreen.thanksToThisYearsSponsors")}
+            </Text>
+          </>
+        }
+        data={SPONSORS_DATA}
+        renderItem={({ item }) => (
+          <View style={$container}>
+            <SponsorSection item={item} />
+          </View>
         )}
       />
     </Screen>
@@ -122,11 +137,11 @@ const $container: ViewStyle = {
 }
 
 const $heading: TextStyle = {
-  fontSize: 32,
-  lineHeight: 45.2,
   backgroundColor: colors.palette.neutral500,
+  paddingHorizontal: spacing.large,
 }
 
 const $carousel: ViewStyle = {
-  marginVertical: spacing.large,
+  marginTop: spacing.extraLarge,
+  marginBottom: spacing.extraLarge * 2,
 }
