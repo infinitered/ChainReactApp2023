bc1d72a73e2a34660c3b6cb1e1cb705f21698ae9
Frank Calise <fcalise@gmail.com>
2022-12-14 10:56:59 -0500

    fix(venue-screen): keep section list, fix styling and nested list warning gone

--- app/screens/VenueScreen/VenueScreen.tsx
+++ app/screens/VenueScreen/VenueScreen.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react"
-import { FlatList, TextStyle, View, ViewStyle } from "react-native"
-import { Carousel, DynamicCarouselItem, Screen, Text } from "../../components"
+import { SectionList, TextStyle, View, ViewStyle } from "react-native"
+import { Carousel, Screen, Text } from "../../components"
 import { TabScreenProps } from "../../navigators/TabNavigator"
 import { colors, spacing } from "../../theme"
 import { useHeader } from "../../hooks/useHeader"
@@ -10,95 +10,106 @@ import { SponsorCard } from "./SponsorCard"
 const irImage1 = require("../../../assets/images/info-ir1.png")
 const irImage2 = require("../../../assets/images/info-ir2.png")
 
-const WORKSHOP_DATA: DynamicCarouselItem[] = [
-  {
-    image: irImage1,
-    subtitle: "The Armory",
-    meta: "Conference • May 18-19",
-    body: "11134 Washington St #302\nPortland, OR 97006",
-    leftButton: {
-      text: "Open in maps",
-      link: "11134 Washington St #302, Portland, OR 97006",
-    },
-  },
-  {
-    image: irImage2,
-    subtitle: "Workshops • May 17",
-    meta: "Marriott Portland City Center",
-    body: "550 SW Oak Street\nPortland, OR 97204",
-    leftButton: {
-      text: "Open in maps",
-      link: "550 SW Oak Street\nPortland, OR 97204",
-    },
-  },
-]
-
-const Workshops = () => {
+const Workshops = ({ item }) => {
   return (
     <View style={$carousel}>
-      <Carousel
-        preset="dynamic"
-        data={WORKSHOP_DATA}
-        title={translate("venueScreen.conferenceAndWorkshopVenues")}
-      />
+      <Carousel preset="dynamic" data={item} />
     </View>
   )
 }
 
 const SponsorSection = ({ item }) => {
-  return <SponsorCard {...item} />
+  return (
+    <View style={$container}>
+      <SponsorCard {...item} />
+    </View>
+  )
 }
 
-const SPONSORS_DATA = [
+const SECTIONS = [
   {
-    sponsor: "Sponsor 1",
-    tier: "platinum",
-    bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
-    openURL: "https://placekitten.com",
-    sponsorImage: {
-      uri: "https://placekitten.com/g/280/60",
-    },
-  },
-  {
-    sponsor: "Sponsor 2",
-    tier: "gold",
-    bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
-    openURL: "https://placekitten.com",
-    sponsorImage: {
-      uri: "https://placekitten.com/g/280/60",
-    },
-  },
-  {
-    sponsor: "Sponsor 3",
-    tier: "gold",
-    bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
-    openURL: "https://placekitten.com",
-    sponsorImage: {
-      uri: "https://placekitten.com/g/280/60",
-    },
-  },
-  {
-    tier: "silver",
-    sponsorImages: [
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+    title: translate("venueScreen.conferenceAndWorkshopVenues"),
+    renderItem: Workshops,
+    data: [
+      [
+        {
+          image: irImage1,
+          subtitle: "The Armory",
+          meta: "Conference • May 18-19",
+          body: "11134 Washington St #302\nPortland, OR 97006",
+          leftButton: {
+            text: "Open in maps",
+            link: "11134 Washington St #302, Portland, OR 97006",
+          },
+        },
+        {
+          image: irImage2,
+          subtitle: "Workshops • May 17",
+          meta: "Marriott Portland City Center",
+          body: "550 SW Oak Street\nPortland, OR 97204",
+          leftButton: {
+            text: "Open in maps",
+            link: "550 SW Oak Street, Portland, OR 97204",
+          },
+        },
+      ],
     ],
   },
   {
-    tier: "bronze",
-    sponsorImages: [
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
-      { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+    title: translate("venueScreen.thanksToThisYearsSponsors"),
+    renderItem: SponsorSection,
+    data: [
+      {
+        sponsor: "Sponsor 1",
+        tier: "platinum",
+        bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
+        openURL: "https://placekitten.com",
+        sponsorImage: {
+          uri: "https://placekitten.com/g/280/60",
+        },
+      },
+      {
+        sponsor: "Sponsor 2",
+        tier: "gold",
+        bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
+        openURL: "https://placekitten.com",
+        sponsorImage: {
+          uri: "https://placekitten.com/g/280/60",
+        },
+      },
+      {
+        sponsor: "Sponsor 3",
+        tier: "gold",
+        bio: "Bio info included with sponsor tier section for website and mobile app. Applies to platinum and gold sponsors.",
+        openURL: "https://placekitten.com",
+        sponsorImage: {
+          uri: "https://placekitten.com/g/280/60",
+        },
+      },
+      {
+        tier: "silver",
+        sponsorImages: [
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats r us" },
+        ],
+      },
+      {
+        tier: "bronze",
+        sponsorImages: [
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+          { uri: "https://placekitten.com/g/155/36", sponsor: "cats rule dog drool" },
+        ],
+      },
     ],
   },
 ]
@@ -108,20 +119,13 @@ export const VenueScreen: FC<TabScreenProps<"Venue">> = () => {
 
   return (
     <Screen style={$root} preset="fixed">
-      <FlatList
-        ListHeaderComponent={
-          <>
-            <Workshops />
-            <Text preset="screenHeading" style={$heading}>
-              {translate("venueScreen.thanksToThisYearsSponsors")}
-            </Text>
-          </>
-        }
-        data={SPONSORS_DATA}
-        renderItem={({ item }) => (
-          <View style={$container}>
-            <SponsorSection item={item} />
-          </View>
+      <SectionList
+        stickySectionHeadersEnabled={false}
+        sections={SECTIONS}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text preset="screenHeading" style={$heading}>
+            {title}
+          </Text>
         )}
       />
     </Screen>
@@ -138,10 +142,10 @@ const $container: ViewStyle = {
 
 const $heading: TextStyle = {
   backgroundColor: colors.palette.neutral500,
+  marginTop: spacing.extraLarge,
   paddingHorizontal: spacing.large,
 }
 
 const $carousel: ViewStyle = {
-  marginTop: spacing.extraLarge,
-  marginBottom: spacing.extraLarge * 2,
+  marginBottom: spacing.extraLarge,
 }
