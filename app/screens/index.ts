096c6dc16858b68d7ffe3e74a6c895abe388c6a5
Mazen Chami <mazenchami@gmail.com>
2023-01-09 11:06:54 -0500

    exporting party details

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,6 +1,7 @@
 export * from "./WelcomeScreen"
 export * from "./ErrorScreen/ErrorBoundary"
 export * from "./PlaceholderScreen"
+export * from "./PartyDetailsScreen/PartyDetailsScreen"
 export * from "./ScheduleScreen/ScheduleScreen"
 export * from "./TalkDetailsScreen/TalkDetailsScreen"
 export * from "./VenueScreen/VenueScreen"
