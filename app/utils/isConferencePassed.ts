ed0926589699881346f3f332a43614f31e10a2c0
Jon Major Condon <jmajorcondon@gmail.com>
2023-02-16 10:25:30 -0600

    update conf is passed to be a func

--- app/utils/isConferencePassed.ts
+++ app/utils/isConferencePassed.ts
@@ -1,3 +1,3 @@
 import isPast from "date-fns/isPast"
 
-export const isConferencePassed = isPast(new Date("2023-05-19T23:59:59")) // After midnight on May 19th, 2023 conference is passed
+export const isConferencePassed = () => isPast(new Date("2023-05-19T23:59:59")) // After midnight on May 19th, 2023 conference is passed
