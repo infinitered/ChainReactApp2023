ee3d7fca969749b019cccef7a3257e1b52c08e4c
Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>
2023-02-17 16:39:48 -0800

    Update app/services/reactotron/reactotron.ts

--- app/services/reactotron/reactotron.ts
+++ app/services/reactotron/reactotron.ts
@@ -109,7 +109,7 @@ export function setupReactotron(customConfig: ReactotronConfig = {}) {
         }
       },
       title: "Set Current Date",
-      description: "Set current date for timeline view",
+      description: "Use any valid Date() string to set the current date for the schedule view. For example: 'Fri May 19 2023 14:00:00 GMT-0800 (Pacific Standard Time)'",
       args: [
         {
           name: "currentDate",
