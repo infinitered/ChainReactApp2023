a8bf7c215f5d88cc64be523e10eeae155a4ef2ad
Mazen Chami <mazenchami@gmail.com>
2023-04-05 14:40:48 -0400

    Fix/multi scroll width (#94)

--- app/components/carousel/constants.ts
+++ app/components/carousel/constants.ts
@@ -2,6 +2,6 @@ import { Dimensions } from "react-native"
 import { spacing } from "../../theme"
 
 const { width: SCREEN_WIDTH } = Dimensions.get("screen")
-export const CAROUSEL_IMAGE_WIDTH = SCREEN_WIDTH * 0.9
+export const CAROUSEL_IMAGE_WIDTH = SCREEN_WIDTH * 0.85
 export const SPACING = spacing.extraSmall
-export const SPACER_WIDTH = (SCREEN_WIDTH - CAROUSEL_IMAGE_WIDTH) / 2
+export const SPACER_WIDTH = (SCREEN_WIDTH - SCREEN_WIDTH * 0.9) / 2
