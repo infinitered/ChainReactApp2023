1d6a86a851cdb44255da2b9dbb5800acb6ac2b6e
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-04-13 20:34:03 -0700

    Fix Text type

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -1,5 +1,5 @@
 import i18n from "i18n-js"
-import React, { forwardRef, Ref } from "react"
+import React, { forwardRef } from "react"
 import { StyleProp, Text as RNText, TextProps as RNTextProps, TextStyle } from "react-native"
 import { isRTL, translate, TxKeyPath } from "../i18n"
 import { colors, typography } from "../theme"
@@ -58,14 +58,14 @@ const letterSpacing = (value) => {
  *
  * - [Documentation and Examples](https://github.com/infinitered/ignite/blob/master/docs/Components-Text.md)
  */
-export const Text = forwardRef(function Text(props: TextProps, ref: Ref<Text>) {
+export const Text = forwardRef<RNText, TextProps>(function Text(props, ref) {
   const { weight, size, tx, txOptions, text, children, style: $styleOverride, ...rest } = props
 
   const i18nText = tx && translate(tx, txOptions)
   const content = i18nText || text || children
 
   const preset: Presets = $presets[props.preset] ? props.preset : "default"
-  const $styles = [
+  const $styles: StyleProp<TextStyle> | undefined = [
     $rtlStyle,
     $presets[preset],
     $fontWeightStyles[weight],
@@ -81,95 +81,127 @@ export const Text = forwardRef(function Text(props: TextProps, ref: Ref<Text>) {
 })
 
 const $sizeStyles = {
-  xxxl: { fontSize: 42, lineHeight: 46.2 } as TextStyle,
-  xxl: { fontSize: 32, lineHeight: 35.2 } as TextStyle,
-  xl: { fontSize: 26, lineHeight: 28.6 } as TextStyle,
-  lg: { fontSize: 22, lineHeight: 24.2 } as TextStyle,
-  md: { fontSize: 18, lineHeight: 19.8 } as TextStyle,
-  sm: { fontSize: 16, lineHeight: 17.6 } as TextStyle,
-  xs: { fontSize: 14, lineHeight: 15.4 } as TextStyle,
-  xxs: { fontSize: 12, lineHeight: 13.2 } as TextStyle,
+  xxxl: { fontSize: 42, lineHeight: 46.2 } satisfies TextStyle,
+  xxl: { fontSize: 32, lineHeight: 35.2 } satisfies TextStyle,
+  xl: { fontSize: 26, lineHeight: 28.6 } satisfies TextStyle,
+  lg: { fontSize: 22, lineHeight: 24.2 } satisfies TextStyle,
+  md: { fontSize: 18, lineHeight: 19.8 } satisfies TextStyle,
+  sm: { fontSize: 16, lineHeight: 17.6 } satisfies TextStyle,
+  xs: { fontSize: 14, lineHeight: 15.4 } satisfies TextStyle,
+  xxs: { fontSize: 12, lineHeight: 13.2 } satisfies TextStyle,
 }
 
 const $fontWeightStyles = Object.entries(typography.primary).reduce((acc, [weight, fontFamily]) => {
   return { ...acc, [weight]: { fontFamily } }
-}, {}) as Record<Weights, TextStyle>
+}, {} as Record<Weights, TextStyle>) satisfies Record<Weights, TextStyle>
 
-export const $baseStyle: StyleProp<TextStyle> = [
+export const $baseStyle = [
   $sizeStyles.sm,
   $fontWeightStyles.book,
   { color: colors.text, lineHeight: 22 },
-]
+] satisfies StyleProp<TextStyle>
 
 const $secondaryFontWeightStyles = Object.entries(typography.secondary).reduce(
   (acc, [weight, fontFamily]) => {
     return { ...acc, [weight]: { fontFamily } }
   },
-  {},
-) as Record<Weights, TextStyle>
+  {} as Record<Weights, TextStyle>,
+) satisfies Record<Weights, TextStyle>
 
-export const $baseSecondaryStyle: StyleProp<TextStyle> = [
+export const $baseSecondaryStyle = [
   $sizeStyles.sm,
   $secondaryFontWeightStyles.book,
   { color: colors.text },
-]
+] satisfies StyleProp<TextStyle>
 
 const $presets = {
   default: $baseStyle,
 
-  bold: [$baseStyle, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  bold: [$baseStyle, $fontWeightStyles.bold] satisfies StyleProp<TextStyle>,
 
-  welcomeHeading: [$baseStyle, $sizeStyles.xxxl, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  welcomeHeading: [
+    $baseStyle,
+    $sizeStyles.xxxl,
+    $fontWeightStyles.bold,
+  ] satisfies StyleProp<TextStyle>,
 
-  screenHeading: [$baseStyle, $sizeStyles.xxl, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  screenHeading: [
+    $baseStyle,
+    $sizeStyles.xxl,
+    $fontWeightStyles.bold,
+  ] satisfies StyleProp<TextStyle>,
 
-  cardFooterHeading: [$baseStyle, $sizeStyles.lg, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  cardFooterHeading: [
+    $baseStyle,
+    $sizeStyles.lg,
+    $fontWeightStyles.bold,
+  ] satisfies StyleProp<TextStyle>,
 
-  companionHeading: [$baseStyle, $sizeStyles.md, $fontWeightStyles.medium] as StyleProp<TextStyle>,
+  companionHeading: [
+    $baseStyle,
+    $sizeStyles.md,
+    $fontWeightStyles.medium,
+  ] satisfies StyleProp<TextStyle>,
 
-  subheading: [$baseStyle, $sizeStyles.lg, $fontWeightStyles.medium] as StyleProp<TextStyle>,
+  subheading: [$baseStyle, $sizeStyles.lg, $fontWeightStyles.medium] satisfies StyleProp<TextStyle>,
 
-  boldHeading: [$baseStyle, $sizeStyles.xl, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  boldHeading: [$baseStyle, $sizeStyles.xl, $fontWeightStyles.bold] satisfies StyleProp<TextStyle>,
 
   infoText: [
     $baseStyle,
     $sizeStyles.xs,
     $fontWeightStyles.book,
     { lineHeight: 22 },
-  ] as StyleProp<TextStyle>,
+  ] satisfies StyleProp<TextStyle>,
 
-  formLabel: [$baseStyle, $fontWeightStyles.medium] as StyleProp<TextStyle>,
+  formLabel: [$baseStyle, $fontWeightStyles.medium] satisfies StyleProp<TextStyle>,
 
-  formHelper: [$baseStyle, $sizeStyles.sm, $fontWeightStyles.book] as StyleProp<TextStyle>,
+  formHelper: [$baseStyle, $sizeStyles.sm, $fontWeightStyles.book] satisfies StyleProp<TextStyle>,
 
-  link: [$baseSecondaryStyle, $sizeStyles.xxs, $secondaryFontWeightStyles.medium],
+  link: [
+    $baseSecondaryStyle,
+    $sizeStyles.xxs,
+    $secondaryFontWeightStyles.medium,
+  ] satisfies StyleProp<TextStyle>,
 
   primaryLabel: [
     $baseSecondaryStyle,
     $sizeStyles.xxs,
     $secondaryFontWeightStyles.medium,
     { color: colors.palette.primary500, textTransform: "uppercase" },
-  ] as StyleProp<TextStyle>,
+  ] satisfies StyleProp<TextStyle>,
 
-  eventTitle: [$baseSecondaryStyle, $sizeStyles.xxs, $secondaryFontWeightStyles.medium],
+  eventTitle: [
+    $baseSecondaryStyle,
+    $sizeStyles.xxs,
+    $secondaryFontWeightStyles.medium,
+  ] satisfies StyleProp<TextStyle>,
 
-  tag: [$baseSecondaryStyle, $sizeStyles.xs, $secondaryFontWeightStyles.medium],
+  tag: [
+    $baseSecondaryStyle,
+    $sizeStyles.xs,
+    $secondaryFontWeightStyles.medium,
+  ] satisfies StyleProp<TextStyle>,
 
-  navHeader: [$baseSecondaryStyle, $sizeStyles.sm, $secondaryFontWeightStyles.medium],
+  navHeader: [
+    $baseSecondaryStyle,
+    $sizeStyles.sm,
+    $secondaryFontWeightStyles.medium,
+  ] satisfies StyleProp<TextStyle>,
 
   listHeading: [
     $baseSecondaryStyle,
     $sizeStyles.sm,
     $secondaryFontWeightStyles.bold,
     { letterSpacing: letterSpacing(8) },
-  ],
+  ] satisfies StyleProp<TextStyle>,
 
   label: [
     $baseSecondaryStyle,
     $sizeStyles.xxs,
     $secondaryFontWeightStyles.medium,
     { letterSpacing: letterSpacing(8), textTransform: "uppercase" },
-  ],
+  ] satisfies StyleProp<TextStyle>,
 }
 
 const $rtlStyle: TextStyle = isRTL ? { writingDirection: "rtl" } : {}
