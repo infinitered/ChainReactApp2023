078954c88e4d1dc891e5a6557e059435247a20e6
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-04-17 15:23:32 -0700

    Add webflow doc comments, add more Webflow field schemas

--- app/services/api/webflow-api.schema.ts
+++ app/services/api/webflow-api.schema.ts
@@ -4,6 +4,7 @@ export const DateStringSchema = z.string()
 
 export type DateString = z.infer<typeof DateStringSchema>
 
+/** @see https://developers.webflow.com/reference/list-collections */
 export const ListCollectionsItemSchema = z.object({
   _id: z.string(),
   lastUpdated: DateStringSchema,
@@ -12,10 +13,13 @@ export const ListCollectionsItemSchema = z.object({
   slug: z.string(),
 })
 
+/** @see https://developers.webflow.com/reference/list-collections */
 export type ListCollectionsItem = z.infer<typeof ListCollectionsItemSchema>
 
+/** @see https://developers.webflow.com/reference/list-collections */
 export const ListCollectionsResponseSchema = z.array(ListCollectionsItemSchema)
 
+/** @see https://developers.webflow.com/reference/list-collections */
 export type ListCollectionsResponse = z.infer<typeof ListCollectionsResponseSchema>
 
 export const ValidationSchema = z.any()
@@ -33,27 +37,7 @@ export const FieldSchema = z.object({
 
 export type Field = z.infer<typeof FieldSchema>
 
-/**
- * @example
-{
-  "_id": "580e63fc8c9a982ac9b8b745",
-  "lastUpdated": "2016-10-24T19:42:38.929Z",
-  "createdOn": "2016-10-24T19:41:48.349Z",
-  "name": "Blog Posts",
-  "slug": "post",
-  "singularName": "Blog Post",
-  "fields": [
-    {
-      "id": "7f62a9781291109b9e428fb47239fd35",
-      "editable": true,
-      "required": false,
-      "type": "RichText",
-      "slug": "post-body",
-      "name": "Post Body"
-    },
-  ]
-}
- */
+/** @see https://developers.webflow.com/reference/get-collection */
 export const GetCollectionResponseSchema = z.object({
   _id: z.string(),
   lastUpdated: DateStringSchema,
@@ -64,9 +48,11 @@ export const GetCollectionResponseSchema = z.object({
   fields: z.array(FieldSchema),
 })
 
+/** @see https://developers.webflow.com/reference/get-collection */
 export type GetCollectionResponse = z.infer<typeof GetCollectionResponseSchema>
 
-export const GetCollectionItemResponseSchema = <T extends z.ZodTypeAny>(itemSchema: T) =>
+/** @see @see https://developers.webflow.com/reference/list-items */
+export const GetCollectionItemsResponseSchema = <T extends z.ZodTypeAny>(itemSchema: T) =>
   z.object({
     items: z.array(itemSchema),
     total: z.number(),
@@ -78,18 +64,41 @@ export const CollectionBaseSchema = z.object({
   _id: z.string(),
   _cid: z.string(),
 })
+/** @see https://developers.webflow.com/docs/field-types#imageref--image */
 export const ImageRefSchema = z.object({
   alt: z.string().optional().nullable(),
   fileId: z.string(),
   url: z.string(),
 })
+/** @see https://developers.webflow.com/docs/field-types#imageref--image */
+export const ImageRefSetSchema = z.array(ImageRefSchema)
+/** @see https://developers.webflow.com/docs/field-types#itemref--reference */
 export const ItemRefSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#itemref--reference */
 export const ItemRefSetSchema = z.array(ItemRefSchema)
+/** @see https://developers.webflow.com/docs/field-types#imagerefset--multi-image */
 export const SetSchema = z.array(ImageRefSchema)
+/** @see https://developers.webflow.com/docs/field-types#plain-text */
 export const PlainTextSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#link */
 export const LinkSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#option */
 export const OptionSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#boolean--switch */
 export const BoolSchema = z.boolean()
+/** @see https://developers.webflow.com/docs/field-types#datetime */
 export const DateSchema = DateStringSchema.nullable()
+/** @see https://developers.webflow.com/docs/field-types#user */
 export const UserSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#rich-text */
 export const RichTextSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#video-link */
+export const VideoLinkSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#email */
+export const EmailSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#phone */
+export const PhoneSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#number */
+export const NumberSchema = z.string()
+/** @see https://developers.webflow.com/docs/field-types#color */
+export const ColorSchema = z.string()
