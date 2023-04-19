60934b41598fedcea816eeac282f737d737fd58f
Mazen Chami <mazenchami@gmail.com>
2023-04-19 16:46:46 -0400

    #135 dont localize times—keep PT

--- app/utils/formatDate.ts
+++ app/utils/formatDate.ts
@@ -14,13 +14,39 @@ const getLocale = (): Locale => {
 
 export const parseDate = (date: string | Date) => (date instanceof Date ? date : parseISO(date))
 
+const parsedDateToPSTDate = (date: string | Date): Date => {
+  const inputDate = parseDate(date)
+  const timeZone = "America/Los_Angeles"
+
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    timeZone,
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  })
+
+  const parts = formatter.formatToParts(inputDate)
+  const year = parseInt(parts.find((part) => part.type === "year")?.value || "", 10)
+  const month = parseInt(parts.find((part) => part.type === "month")?.value || "", 10) - 1 // Months are 0-indexed in JavaScript
+  const day = parseInt(parts.find((part) => part.type === "day")?.value || "", 10)
+  const hour = parseInt(parts.find((part) => part.type === "hour")?.value || "", 10)
+  const minute = parseInt(parts.find((part) => part.type === "minute")?.value || "", 10)
+  const second = parseInt(parts.find((part) => part.type === "second")?.value || "", 10)
+
+  return new Date(year, month, day, hour, minute, second)
+}
+
 export const formatDate = (date: string | Date, dateFormat?: string, options?: Options) => {
   const locale = getLocale()
   const dateOptions = {
     ...options,
     locale,
   }
-  return format(parseDate(date), dateFormat ?? "MMM dd, yyyy", dateOptions)
+  return format(parsedDateToPSTDate(date), dateFormat ?? "MMM dd, yyyy", dateOptions)
 }
 
 type DayTime = { ["day-time"]: Date | string }
