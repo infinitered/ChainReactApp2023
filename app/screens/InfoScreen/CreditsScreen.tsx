0caad88a06c41933d07e95091cfc1c9ca3c3b5cc
Nick Morgan <hello@morganick.com>
2023-04-20 14:25:57 -0400

    Updating credits and extracting constant out of component

--- app/screens/InfoScreen/CreditsScreen.tsx
+++ app/screens/InfoScreen/CreditsScreen.tsx
@@ -6,6 +6,102 @@ import { useAppNavigation, useHeader } from "../../hooks"
 import { colors, spacing } from "../../theme"
 import { FlashList } from "@shopify/flash-list"
 
+type Person = {
+  name: string
+  role: string
+}
+
+const data: Person[] = [
+  {
+    name: "Gant Laborde",
+    role: "Organizer of Chain React",
+  },
+  {
+    name: "Justin Huskey",
+    role: "Co-organizer of Chain React & App Design",
+  },
+  {
+    name: "Jamon Holmgren",
+    role: "Co-organizer of Chain React & App Development",
+  },
+  {
+    name: "Jenna Fucci",
+    role: "Designer (web, app, and print)",
+  },
+  {
+    name: "Simran Sachdeva",
+    role: "CFP Committee, Workshop Assistant, & App Testing",
+  },
+  {
+    name: "Darin Wilson",
+    role: "CFP Committe",
+  },
+  {
+    name: "Josh Yoes",
+    role: "CFP Committee & App Development",
+  },
+  {
+    name: "Kate Kim",
+    role: "CFP Committee & App Development",
+  },
+  {
+    name: "Robin Heinze",
+    role: "App Lead & IR Table",
+  },
+  {
+    name: "Mazen Chami",
+    role: "App Development",
+  },
+  {
+    name: "Frank Calise",
+    role: "App Development",
+  },
+  {
+    name: "Leon Kim",
+    role: "App Development & Testing",
+  },
+  {
+    name: "Silas Matson",
+    role: "App Development",
+  },
+  {
+    name: "Jon Major Condon",
+    role: "App Development",
+  },
+  {
+    name: "Yulian Glukhenko",
+    role: "App Development",
+  },
+  {
+    name: "Nick Morgan",
+    role: "App Development",
+  },
+  {
+    name: "Bryan Sterns",
+    role: "App Testing",
+  },
+  {
+    name: "Derek Greenberg",
+    role: "App Testing",
+  },
+  {
+    name: "Dan Edwards",
+    role: "App Testing",
+  },
+  {
+    name: "Jed Bartausky",
+    role: "App Testing",
+  },
+  {
+    name: "Ellie Croce",
+    role: "App Testing",
+  },
+  {
+    name: "Trevor Coleman",
+    role: "App Testing",
+  },
+]
+
 export const CreditsScreen = () => {
   const navigation = useAppNavigation()
   const { width } = Dimensions.get("window")
@@ -15,98 +111,6 @@ export const CreditsScreen = () => {
     leftIcon: "back",
     onLeftPress: () => navigation.goBack(),
   })
-
-  type Person = {
-    name: string
-    role: string
-  }
-
-  const data: Person[] = [
-    {
-      name: "Gant Laborde",
-      role: "Organizer of Chain React",
-    },
-    {
-      name: "Justin Huskey",
-      role: "Co-organizer of Chain React & App Design",
-    },
-    {
-      name: "Jamon Holmgren",
-      role: "Co-organizer of Chain React & App Development",
-    },
-    {
-      name: "Jenna Fucci",
-      role: "Designer (web, app, and print)",
-    },
-    {
-      name: "Simran Sachdeva",
-      role: "CFP Committee & Workshop Assistant",
-    },
-    {
-      name: "Darin Wilson",
-      role: "CFP Committe",
-    },
-    {
-      name: "Josh Yoes",
-      role: "CFP Committee & App Development",
-    },
-    {
-      name: "Kate Kim",
-      role: "CFP Committee & App Development",
-    },
-    {
-      name: "Robin Heinze",
-      role: "App Lead & IR Table",
-    },
-    {
-      name: "Mazen Chami",
-      role: "App Development",
-    },
-    {
-      name: "Frank Calise",
-      role: "App Development",
-    },
-    {
-      name: "Leon Kim",
-      role: "App Development",
-    },
-    {
-      name: "Silas Matson",
-      role: "App Development",
-    },
-    {
-      name: "Jon Major Condon",
-      role: "App Development",
-    },
-    {
-      name: "Yulian Glukhenko",
-      role: "App Development",
-    },
-    {
-      name: "Nick Morgan",
-      role: "App Development",
-    },
-    {
-      name: "Bryan Sterns",
-      role: "App Testing",
-    },
-    {
-      name: "Derek Greenberg",
-      role: "App Testing",
-    },
-    {
-      name: "Dan Edwards",
-      role: "App Testing",
-    },
-    {
-      name: "Jed Bartausky",
-      role: "App Testing",
-    },
-    {
-      name: "Ellie Croce",
-      role: "App Testing",
-    },
-  ]
 
   return (
     <Screen style={$root}>
