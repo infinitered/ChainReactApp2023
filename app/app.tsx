c581877db5b5b125e605539411f5e9a1d66be5a6
Mazen Chami <mazenchami@gmail.com>
2023-04-28 08:20:20 -0400

    reverting app.tsx updates

--- app/app.tsx
+++ app/app.tsx
@@ -12,7 +12,7 @@
 import "./i18n"
 import "./utils/ignoreWarnings"
 import { useFonts } from "expo-font"
-import React, { useLayoutEffect } from "react"
+import React, { useEffect, useLayoutEffect } from "react"
 import {
   initialWindowMetrics,
   SafeAreaProvider,
@@ -104,11 +104,15 @@ function App(props: AppProps) {
   useLayoutEffect(() => {
     // hide splash screen after 500ms
     setTimeout(hideSplashScreen, 500)
-    // if recovered from error, reset the state
+  })
+
+  useLayoutEffect(() => {
     if (recoveredFromError) {
       setRecoveredFromError(false)
     }
-    // OTA updates
+  })
+
+  useEffect(() => {
     async function onFetchUpdateAsync() {
       try {
         const update = await Updates.checkForUpdateAsync()
@@ -122,7 +126,7 @@ function App(props: AppProps) {
       }
     }
     onFetchUpdateAsync()
-  })
+  }, [])
 
   // Before we show the app, we have to wait for our state to be ready.
   // In the meantime, don't render anything. This will be the background
