5af95e533f81c4ae567b624b6b7eef4ad9a556eb
Nick Morgan <hello@morganick.com>
2023-05-01 16:08:28 -0400

    Updating build number and instance type

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config"
 import { version } from "./package.json"
 
-const BUILD_NUMBER = 2
+const BUILD_NUMBER = 3
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
